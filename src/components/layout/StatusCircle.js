92d3679aab20df9430aec68acc1b0b7ddece61e8
Emiel Van Severen <emielvanseveren@outlook.com>
2019-11-16 12:06:24 +0000

    Basic state is now shown in circle

--- src/components/layout/StatusCircle.js
+++ src/components/layout/StatusCircle.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import styled from 'styled-components'
 
 const Circle = styled.div`
@@ -11,31 +11,35 @@ const Circle = styled.div`
 `
 
 export default function StatusCircle(){
-  // 3 states, active, issues, down
+  // #000 = unkown as it is not fetched yet
+  const [ss, setServerState] = useState('#000000')
+
+  useEffect(() => {
+    showState()
+  }, [])
+
   async function showState(){
     // fetch here
-    const usRes = await fetch('https://us.csmm.app/api/stats')
-    const usData = await usRes.json()
+    const urls = ['https://us.csmm.app/api/stats','https://eu.csmm.app/api/stats','https://au.csmm.app/api/stats']
+    let serversUp = 0
+    for (let i=0; i<urls.length; i++){
+      const data = await fetch(urls[i])
+      const json = await data.json()
+      serversUp += json.length // max return value is 150 (50 per region)
+    }
+    console.log(serversUp)
 
-    const euRes = await fetch('https://eu.csmm.app/api/stats')
-    const euData = await euRes.json()
-
-    const auRes = await fetch('https://au.csmm.app/api/stats')
-    const auData = await auRes.json()
-    
-    console.log(auRes)
-
-    const state = 'active'
-    switch (state){
-      case 'active':
-        return (<Circle color="#28B766"/>)
-      case 'issues':
-        return <Circle color="#FF8C00"/>
-      case 'down':
-        return <Circle color="#DC143C"/>
+    if(serversUp > 125){
+      setServerState('#28B766')
+    }
+    if(serversUp < 125 && serversUp > 100){
+      setServerState('#FF4500')
+    }
+    if(serversUp < 100){
+      setServerState('#DC143C')
     }
   }
   return (
-    showState()
+    <Circle color={ss} />
   )
 }
