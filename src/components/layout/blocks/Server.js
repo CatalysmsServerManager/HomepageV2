39b7605d6b9c422a96afc06ebeaedd252d3a5474
Emiel Van Severen <emielvanseveren@outlook.com>
2019-11-16 14:12:28 +0000

    Add start server component

---
+++ src/components/layout/blocks/Server.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.section`
+  margin: 150px 0;
+`
+
+export default function Server(){
+  return (
+    <Container>this is the server</Container>
+  )
+}
