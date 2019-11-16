ab2daa7a23e6e7dffb0b77e631d66f0a77de5884
Emiel Van Severen <emielvanseveren@outlook.com>
2019-11-16 11:43:32 +0000

    Add fetches for server status

--- src/components/layout/StatusCircle.js
+++ src/components/layout/StatusCircle.js
@@ -12,12 +12,23 @@ const Circle = styled.div`
 
 export default function StatusCircle(){
   // 3 states, active, issues, down
-  function showState(){
+  async function showState(){
     // fetch here
+    const usRes = await fetch('https://us.csmm.app/api/stats')
+    const usData = await usRes.json()
+
+    const euRes = await fetch('https://eu.csmm.app/api/stats')
+    const euData = await euRes.json()
+
+    const auRes = await fetch('https://au.csmm.app/api/stats')
+    const auData = await auRes.json()
+    
+    console.log(auRes)
+
     const state = 'active'
     switch (state){
       case 'active':
-        return <Circle color="#28B766"/>
+        return (<Circle color="#28B766"/>)
       case 'issues':
         return <Circle color="#FF8C00"/>
       case 'down':
