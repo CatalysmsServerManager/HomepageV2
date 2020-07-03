06977086de85df9003814308ec45b4bf675b260d
Emiel Van Severen <emielvanseveren@outlook.com>
2020-07-03 13:24:35 +0200

    Fix colors in statistics text were reversed

--- src/components/home/statistics/style.ts
+++ src/components/home/statistics/style.ts
@@ -24,13 +24,13 @@ export const TextContainer = styled.div`
 
   .players {
     display: inline;
-    color: ${({ theme }): string => theme.title};
+    color: ${({ theme }): string => theme.main};
     font-weight: 700;
     border-bottom: 3px solid ${({ theme }): string => theme.title}};
   }
   .servers {
     display: inline;
-    color: ${({ theme }): string => theme.main};
+    color: ${({ theme }): string => theme.title};
     font-weight: 700;
     border-bottom: 3px solid ${({ theme }): string => theme.main}};
   }
